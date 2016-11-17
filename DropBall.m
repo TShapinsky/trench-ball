55fb3bb8d3425d06cc06e4fc21c0af93893807d5
rowansharman <rowansharman1@gmail.com>
2016-11-17 17:20:22 -0500

    First pass model

---
+++ DropBall.m
@@ -0,0 +1,36 @@
+function [ depths, times ] = DropBall(diameterBall, densityBall)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+rEarth = 6378e3;
+nominalG = 9.80665;
+
+    function[flows] = dropFun(~, stocks)
+        depth = stocks(1);
+        velocity = stocks(2);
+        
+        radius = diameterBall/2;
+        densityWater = 1000;%getDensity(depth);
+        r = rEarth - depth;
+        g = (rEarth/r)^2 * nominalG;
+        area = pi * radius^2;
+        volumeBall = (4/3) * pi * radius^3;
+        massBall = densityBall * volumeBall;
+       
+        forceDrag = (1/2)* densityWater * velocity^2 * .5 * area;
+        forceGravity = massBall * g;
+        
+        forceBuoyant = volumeBall*densityWater;
+        
+        forceEffective = forceGravity - forceDrag - forceBuoyant;
+        
+        
+        acceleration = forceEffective/massBall;
+        
+        flows = [velocity; acceleration];
+    end
+options = odeset('RelTol',1e-2);
+[times, depths] = ode45(@dropFun, [0,60], [0,0],options);
+end
+
